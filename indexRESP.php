7768656b7135b0c787197440ba9a10659965c335
juanvilu <juanvilu@gmail.com>
2013-03-20 23:55:44 -0600

    Primer Repositorio JADE

---
+++ indexRESP.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php $_SERVER['SERVER_NAME'];?></title>
+<style>	
+	body{
+		background-color: #000;
+	}
+	.SERVER_NAME{		
+		color: #ddd;
+		display: table;
+		font-weight: 600;
+		font-size: 2.5em;
+		margin: 0 auto 10px auto;
+	}
+	img{
+		display: block;
+		margin: 0 auto;
+		max-width: 80%;
+	    min-width: 80%;
+	    height: auto;
+	    width: auto\9; /* ie8 */
+	}
+</style>
+</head>
+<body>
+	<span class="SERVER_NAME"><?php echo $_SERVER['SERVER_NAME'];?></span>
+	<img src="paginaEnConstruccion.jpg">
+</body>
+</html>
